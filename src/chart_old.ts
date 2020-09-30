5ebf86a73c62bd1c7949c6f34d548998a47daad3
David Schach <schachdavid@web.de>
2020-09-30 22:56:11 +0200

    fixed jumping during node drag

--- src/chart_old.ts
+++ src/chart_old.ts
@@ -49,24 +49,17 @@ export const renderChart = async () => {
     };
   });
 
-  const dragStarted = (d: any) => {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
-    console.log('started..');
-  };
-
-  const dragged = (d: any) => {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
-  };
-
-  const dragEnded = (d: any) => {
-    if (!d3.event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
-    console.log('ended..');
-  };
+  //prepare dragging
+  const simulation = d3
+    .forceSimulation()
+    .force(
+      'link',
+      d3.forceLink().id(function (d) {
+        return d.index.toString();
+      })
+    )
+    .force('charge', d3.forceManyBody())
+    .force('center', d3.forceCenter(width / 2, height / 2));
 
   //draw links
   let maxWeight = 0;
@@ -90,14 +83,7 @@ export const renderChart = async () => {
     .selectAll('g')
     .data(nodes)
     .enter()
-    .append('g')
-    .call(
-      d3
-        .drag()
-        .on('start', dragStarted)
-        .on('drag', dragged)
-        .on('end', dragEnded)
-    );
+    .append('g');
 
   let maxNoOfEdges = 0;
   nodes.forEach((node) => {
@@ -105,17 +91,24 @@ export const renderChart = async () => {
     maxNoOfEdges = Math.max(maxNoOfEdges, noOfEdges);
   });
 
-  //prepare dragging
-  const simulation = d3
-    .forceSimulation()
-    .force(
-      'link',
-      d3.forceLink().id(function (d) {
-        return d.index.toString();
-      })
-    )
-    .force('charge', d3.forceManyBody())
-    .force('center', d3.forceCenter(width / 2, height / 2));
+  const dragStarted = (d: any) => {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+    console.log('started..');
+  };
+
+  const dragged = (d: any) => {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  };
+
+  const dragEnded = (d: any) => {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+    console.log('ended..');
+  };
 
   //draw nodes
   const scaleCircles = d3
@@ -128,7 +121,14 @@ export const renderChart = async () => {
     .attr('r', (d) => scaleCircles(getNoOfEdgesForNode(d, edges)))
     .attr('fill', function (d) {
       return '#59ba88';
-    });
+    })
+    .call(
+      d3
+        .drag()
+        .on('start', dragStarted)
+        .on('drag', dragged)
+        .on('end', dragEnded)
+    );
 
   //draw node labels
   const lables = node
@@ -164,6 +164,8 @@ export const renderChart = async () => {
 
   simulation.nodes(<any>nodes).on('tick', ticked);
 
+  //search
+  console.log(d3.select('#search').node());
   d3.select('#search').on('change textInput input', () => {
     const searchString = d3.event.target.value;
     circles.attr('fill', (d) => {
@@ -192,12 +194,29 @@ export const renderChart = async () => {
 
   d3.select(window).on('wheel', handleZoom);
 
+  //position change
+  let spaceKeyDown = false;
+  const handleSpaceKeyDown = () => {
+    if (!spaceKeyDown && d3.event.keyCode === 32) {
+      spaceKeyDown = true;
+      document.body.style.cursor = 'move';
+    }
+  };
+
+  const handleSpaceKeyUp = () => {
+    if (spaceKeyDown && d3.event.keyCode === 32) {
+      spaceKeyDown = false;
+      document.body.style.cursor = 'auto';
+    }
+  };
+
   let isAdjustingPosition = false;
   let x = 0;
   let y = 0;
 
   const handleMouseDown = () => {
     const e = <MouseEvent>d3.event;
+    e.preventDefault();
     x = e.offsetX;
     y = e.offsetY;
     isAdjustingPosition = true;
@@ -214,7 +233,7 @@ export const renderChart = async () => {
   const handlePositionChange = () => {
     const e = <MouseEvent>d3.event;
     e.preventDefault();
-    if (isAdjustingPosition) {
+    if (spaceKeyDown && isAdjustingPosition) {
       const viewboxX = chart.viewBox.animVal.x + (x - e.offsetX) * moveFactor;
       const viewboxY = chart.viewBox.animVal.y + (y - e.offsetY) * moveFactor;
       const height = chart.viewBox.animVal.height;
@@ -228,6 +247,8 @@ export const renderChart = async () => {
     }
   };
 
+  d3.select(window).on('keydown', handleSpaceKeyDown);
+  d3.select(window).on('keyup', handleSpaceKeyUp);
   d3.select(window).on('mousedown', handleMouseDown);
   d3.select(window).on('mouseup', handleMouseUp);
   d3.select(window).on('mousemove', handlePositionChange);
